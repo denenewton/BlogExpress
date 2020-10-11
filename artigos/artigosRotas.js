9220c4c9a3a8c4b50697aa35ac1f3aaa82434ccc
Daniel dos Santos Araujo <denenewton@icloud.com>
2020-10-11 09:29:28 -0300

    add

--- artigos/artigosRotas.js
+++ artigos/artigosRotas.js
@@ -1,8 +1,38 @@
 const express = require('express')
 const router = express.Router()
+const Categoria = require('../categorias/Categoria')
+const Artigo = require('./Artigo')
+const slugify = require('slugify')
 
-router.get('/artigos', (req, res)=>{
-    res.send('Rota de Artigos...')
+router.get('/admin/artigos', (req, res)=>{
+    Artigo.findAll().then((artigos)=>{
+        res.render('admin/artigos/index', { artigos: artigos})
+    })
+})
+
+router.get('/admin/artigos/new', (req, res) =>{
+    Categoria.findAll().then((categorias)=>{
+
+        res.render('admin/artigos/new', { categorias: categorias})
+    })    
+  
+})
+
+router.post('/artigos/save', (req, res)=>{
+    var titulo =req.body.titulo;
+    var corpo = req.body.corpo;
+    var categoia = req.body.categoia;
+
+    Artigo.create({
+        titulo: titulo,
+        slug: slugify(titulo).toLowerCase(),
+        corpo: corpo,
+        categoiaId: categoia
+    }).then(()=>{
+        res.redirect('/admin/artigos')
+    })
+
+
 })
 
 
